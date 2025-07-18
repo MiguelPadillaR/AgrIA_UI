0f04328f59d818387b48048f090c2b876dc050b8
MiguelPadillaR <m.padi.r@icloud.com>
2025-07-18 08:37:10 +0200

    Removed help section...

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { ChatComponent } from './components/chat/chat.component';
 import { ParcelFinderComponent } from './components/parcel-finder/parcel-finder.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
-import { HelpGuideComponent } from './components/help-guide/help-guide.component';
 
 export const routes: Routes = [
     {
@@ -20,12 +19,5 @@ export const routes: Routes = [
         component: ChatComponent,
         title: "AgrIA"
     },
-    {
-        path: "help",
-        component: HelpGuideComponent,
-        title: "Help"
-    },
-
-
     
 ];
